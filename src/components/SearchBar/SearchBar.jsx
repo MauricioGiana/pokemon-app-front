61444b3a8bd9c526c3bd4e0eb0423fb756abedff
Mauricio Giana <jmauricio@hotmail.com.ar>
2022-03-07 10:25:20 -0300

    searchbar bug

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -24,6 +24,7 @@ export default function SearchBar() {
             setStyleNames({
                 ...styleNames,
                 x: styles.hidden,
+                hidebtn: styles.hidebtn,
             });
             navigate("/pokemons");
         } else {
